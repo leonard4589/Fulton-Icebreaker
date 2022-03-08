feb881ff8ba674eced994b1e114974dd453a43cc
TheDarkGammers <bobafett7788@gmail.com>
2022-03-08 10:24:19 -0600

    I am such a goldfish

--- Icebreaker.js
+++ Icebreaker.js
@@ -23,7 +23,7 @@ function populatePage (i){
   ageDiv.innerHTML = "Age: " + students[i].age;
 
   var birthdayDiv = document.getElementById("birthday");
-  birthdayDiv.innerHTML = "Birthday: " students[i].birthday;
+  birthdayDiv.innerHTML = "Birthday: " + students[i].birthday;
 
   var bioDiv = document.getElementById("studentbio");
   bioDiv.innerHTML = students[i].bio;
