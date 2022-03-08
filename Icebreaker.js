da552028e4b7ae905a991e529d7fa6f315119b38
TheDarkGammers <bobafett7788@gmail.com>
2022-03-08 09:53:41 -0600

    function and testdb

--- Icebreaker.js
+++ Icebreaker.js
@@ -3,16 +3,18 @@ b.addEventListener("click", function(){
   alert("Great now make that other page");
 })
 
+function populatePage (i){
 
-i=0
-var nameDiv = document.getElementById("studentname");
-nameDiv.innerHTML = students[i].name;
+  var nameDiv = document.getElementById("studentname");
+  nameDiv.innerHTML = students[i].name;
 
-var gradeDiv = document.getElementById("studentgrade");
-gradeDiv.innerHTML = "Grade: " + students[i].grade;
+  var gradeDiv = document.getElementById("studentgrade");
+  gradeDiv.innerHTML = "Grade: " + students[i].grade;
 
-var ageDiv = document.getElementById("studentage");
-ageDiv.innerHTML = "Age: " + students[i].age;
+  var ageDiv = document.getElementById("studentage");
+  ageDiv.innerHTML = "Age: " + students[i].age;
 
-var bioDiv = document.getElementById("studentbio");
-bioDiv.innerHTML = students[i].bio;
+  var bioDiv = document.getElementById("studentbio");
+  bioDiv.innerHTML = students[i].bio;
+}
+populatePage(0)
