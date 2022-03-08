8f568f0d6347b6552add673ea6f43d1ab0e86016
TheDarkGammers <bobafett7788@gmail.com>
2022-03-08 09:11:55 -0600

    java script

---
+++ Icebreaker.js
@@ -0,0 +1,34 @@
+b=document.getElementById("backbutton");
+b.addEventListener("click", function(){
+  alert("Great now make that other page");
+})
+</script>
+<script type="text/javascript">
+var students = [
+{
+  id: 1,
+  name: 'Holden "Holdie" Appell',
+  grade: 9,
+  age: 95,
+  bio: "I like breathing."
+},
+{
+  id: 2,
+  name: 'Ruthie "Ru" Parham',
+  grade: 10,
+  age: 16,
+  bio: "I like food."
+},
+]
+i=0
+var nameDiv = document.getElementById("studentname");
+nameDiv.innerHTML = students[i].name;
+
+var gradeDiv = document.getElementById("studentgrade");
+gradeDiv.innerHTML = "Grade: " + students[i].grade;
+
+var ageDiv = document.getElementById("studentage");
+ageDiv.innerHTML = "Age: " + students[i].age;
+
+var bioDiv = document.getElementById("studentbio");
+bioDiv.innerHTML = students[i].bio;
