4d25082f86d993d9d1dbc18434cc0ef25dfb88a3
TheDarkGammers <bobafett7788@gmail.com>
2022-03-08 09:38:20 -0600

    made students.js file

--- Icebreaker.js
+++ Icebreaker.js
@@ -3,22 +3,7 @@ b.addEventListener("click", function(){
   alert("Great now make that other page");
 })
 
-var students = [
-{
-  id: 1,
-  name: 'Holden "Holdie" Appell',
-  grade: 9,
-  age: 95,
-  bio: "I like breathing."
-},
-{
-  id: 2,
-  name: 'Ruthie "Ru" Parham',
-  grade: 10,
-  age: 16,
-  bio: "I like food."
-},
-]
+
 i=0
 var nameDiv = document.getElementById("studentname");
 nameDiv.innerHTML = students[i].name;
---
+++ students.js
@@ -0,0 +1,16 @@
+var students = [
+  {
+    id: 1,
+    name: 'Holden "Holdie" Appell',
+    grade: 9,
+    age: 95,
+    bio: "I like breathing."
+  },
+  {
+    id: 2,
+    name: 'Ruthie "Ru" Parham',
+    grade: 10,
+    age: 16,
+    bio: "I like food."
+  },
+]
